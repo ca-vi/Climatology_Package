89349edc444818212eef352af61bdf530c589990
ca-vi <carsten.toad@googlemail.com>
2014-05-08 17:02:38 +0200

    renaming and writing a first (acceptable) Documentation

---
+++ R/read.loggerdat.R
@@ -0,0 +1,14 @@
+read.loggerdat <- function (file, info=TRUE) {
+  
+  data <- read.csv(file, header = FALSE, skip = 4)
+  names(data) <- names(read.csv(file, skip = 1))
+  if (!is.numeric(data$RECORD[1])){
+    cat("ERROR in data. Review the data!")
+  } else {
+  
+  data$TIMESTAMP <- strptime(data$TIMESTAMP, format="%Y-%m-%d %H:%M:%S")
+  
+  if (info==TRUE) print(str(data))  
+  return(data)
+  }
+}
