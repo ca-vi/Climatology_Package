ec46cb25da5ebd012391dfedb2a0fb8a04961211
Mono Surround <akrug@mailbox.tu-berlin.de>
2014-07-06 13:05:53 +0200

    Version 0.4 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

---
+++ R/readENVImet.R
@@ -0,0 +1,79 @@
+###########################
+# Name
+#  
+# readENVImetData
+#
+# Description
+#  
+# Reads the ENVI-met output and creates a datacontainer with the dimensions X, Y, Z, Variable.
+# It checks if the ENVI-met-Output is in Version 3 or in Version 4
+#
+# Usage
+# 
+# output data <- readENVImetData(_path to metafile,_path to datafile_)
+#  
+# Example
+#
+# Input
+#  
+# path of .EDI-File (metafile) and path of .EDT-file (datafile)
+#
+# Output
+#  
+# initialize functions
+#
+# Author
+#  
+# AlK
+#
+# Date
+#  
+# 08.05.2014
+#
+# TODO
+#   
+# - Another function for reading the whole data-folder or simulation?
+# - input-Vector for selected Variables?
+# - ...
+##################################
+# new
+# input parameter from ENVImet-Metafile
+
+
+readENVImetData <- function(metafile, datafile, EMindex) {
+  
+  meta=readLines(metafile)
+  
+  versioncontrol <-grepl("CreatedwithENVI-metV3",gsub(" ","",meta[length(meta)]))
+  
+  if (versioncontrol==TRUE) {
+    
+    cat("ENVI-met V3 ") 
+    cat(meta[1]) # V3.1
+    
+    n_x_grids = strtoi(meta[2])
+    n_y_grids = strtoi(meta[3])
+    n_z_grids = strtoi(meta[4])
+    n_variables = strtoi(meta[5])
+#    n_variables = length(EMindex)
+  }
+  else {
+    
+    cat("ENVI-met V4 ")
+    cat(meta[2]) # V4
+    
+    n_x_grids = strtoi(meta[3])
+    n_y_grids = strtoi(meta[4])
+    n_z_grids = strtoi(meta[5])
+    n_variables = strtoi(meta[6])
+#    n_variables = length(EMindex)
+    
+  }
+  dim=c(n_x_grids,n_y_grids,n_z_grids,n_variables)
+  
+  EMrawData = readBin(datafile, what="numeric", n=file.info(datafile)$size, size=4)
+  EMdata=array(EMrawData, dim=dim )
+  EMdata=EMdata[,,,EMindex]
+  
+  cat(" OK Variables read:",length(EMindex),"\n")
+  return(EMdata)  }
---
+++ R/read_biomet.R
@@ -0,0 +1,42 @@
+############################################################
+######### Rohdaten der BIOMET-Station einlesen #############
+#################### und Aggregation #######################
+
+# written by Alex Krug 2013
+
+# Beta Version: read_in function for biomet station
+# this function needs file pathes of the three input files of the biomet-station
+
+
+read_BIOMET <- function (file_bm1, file_bm2, file_wm, all=F){ 
+  
+  if (any(file.exists(file_bm1) , file.exists(file_bm2) , file.exists(file_wm))==TRUE) {
+      message('All Files were loaded') }
+      else {stop('Please check your input files')}
+
+  # Reading data from file
+  data_BM1 <- read.table(file_bm1, sep =",", dec=".",na.strings=NA, skip=4, col.names=scan(file_bm1, skip=1, nlines=1, sep=",", what=character()))
+  data_BM2 <- read.table(file_bm2, sep =",", dec=".",na.strings=NA, skip=4, col.names=scan(file_bm2, skip=1, nlines=1, sep=",", what=character()))
+  data_WM <- read.table(file_wm, sep =",", dec=".",na.strings=NA, skip=4, col.names=scan(file_wm, skip=1, nlines=1, sep=",", what=character()))
+    
+  #merge tables
+  data1=merge(data_BM1,data_BM2, by="TIMESTAMP", all.x=T,)
+  data=merge(data1,data_WM, by="TIMESTAMP", all.x=T)
+  
+  # Calculate LW_west and add
+  LW_west <- data$LW_west_meas + (5.67e-8 * ((data$T_CNR4_SN121043 + 273.15)^4)) 
+  data <- cbind(data,LW_west)
+  
+  if (all==T) {
+    return(data)} 
+    else {    
+      sel = c('TIMESTAMP', 'SW_downwelling', 'SW_upwelling','SW_east','SW_west','SW_south', 'SW_north', 
+              'LW_downwelling', 'LW_upwelling', 'LW_east','LW_west','LW_south', 'LW_north', 'Ta', 'RH', 
+              'Tglobe', 'Tglobe_7011', 'Wind_Speed', 'Wind_Direction')
+      data <- data[sel]
+      return(data)
+    }
+    
+cat('Done :-)')
+}
+
