e0f49bc6c23aaf902680d882fa13f76d38a1f4a5
ca-vi <carsten.toad@googlemail.com>
2015-02-18 17:13:16 +0100

    more errors...

--- R/read.logger.R
+++ R/read.logger.R
@@ -1,6 +1,6 @@
-read.logger <- function (file, info=FALSE, row.names = 2, ...) {
+read.logger <- function (file, info=FALSE, ...) {
   
-  data <- read.csv(file, header = FALSE, skip = 4, na.strings="NAN", ...)
+  data <- read.csv(file, header = FALSE, skip = 4, na.strings="NAN" row.names = 2, ...)
   names(data) <- names(read.csv(file, skip = 1))
   if (!exists(data$TIMESTAMP | data$RECORD)){
     stop("Could not read the data properly. Please check the head of table in the file.")
