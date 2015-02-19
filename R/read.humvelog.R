257e08d25e57e17e537e362af19b4f55cab39dcd
ca-vi <carsten.toad@googlemail.com>
2015-02-19 10:46:19 +0100

    testing changes

--- R/read.humvelog.R
+++ R/read.humvelog.R
@@ -1,6 +1,6 @@
 read.humvelog <- function ( file, info=TRUE, ...) {
   data <- read.csv2 (file, skip=5, ...)
-  if(!exists(data$beginn) | !exists(data$ende)) stop("Could not read file properly. Please check for wrong number of rows. read.csv2(skip=5)")
+  if(!exists("beginn", where = data) | !exists("ende", where = data)) stop("Could not read file properly. Please check for wrong number of rows. read.csv2(skip=5)")
   datum <- as.Date(read.csv2 (file)[2,6],format="%d.%m.%Y")
   if(sum(is.na(data$beginn)) == length(data$beginn)) stop("Keine Beginn-Zeit lesbar.")
   if(sum(is.na(data$ende)) == length(data$ende)) stop("Keine Ende-Zeit lesbar.")
