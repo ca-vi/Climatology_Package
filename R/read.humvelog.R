5464b110995a423a39b9e0c2aa1972e9bf687eb6
ca-vi <carsten.toad@googlemail.com>
2015-12-10 14:21:23 +0100

    last changes

--- R/read.humvelog.R
+++ R/read.humvelog.R
@@ -5,7 +5,7 @@ read.humvelog <- function ( file, info=TRUE, ...) {
   datum <- as.numeric(datum[1,1])
   
   data <- XLConnect::readWorksheet(wb, sheet = "Humve", startRow = 7)[,-c(4,5,6)]
-  names(data) <- c(names(data)[1:3], "KT19", names(data[5]))
+  names(data) <- c(names(data)[1:3], "KT19", names(data[-(1:4)]))
   
   data$Beginn <- data$Beginn + datum + 2209078800
   data$Ende <- data$Ende + datum + 2209078800
